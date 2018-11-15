edeecd862c88b91c6dd52847f35ff1e06a4eaee6
Ubuntu <azureops@table5radevopsvm.4l3lugxq3xcudo0ibo4qljcaff.gx.internal.cloudapp.net>
2018-11-15 22:16:38 +0000

    udpate hosts script

--- azure/hosts.sh
+++ azure/hosts.sh
@@ -2,5 +2,5 @@
 
 vm_ip=$(terraform output vm_ip)
 echo $vm_ip
-echo "host1 ansible_ssh_port=22 ansible_ssh_host="$vm_ip > inventory
+echo "host1 ansible_user=azureops ansible_ssh_host="$vm_ip > /ssh/inventory
 
