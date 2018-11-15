d8de2a232295ffc74409c3ea58f77d29c5c72688
Ubuntu <azureops@table5radevopsvm.4l3lugxq3xcudo0ibo4qljcaff.gx.internal.cloudapp.net>
2018-11-15 21:30:07 +0000

    open port 80

--- azure/network.tf
+++ azure/network.tf
@@ -56,13 +56,13 @@ resource "azurerm_network_security_group" "mynsg"
   }
   security_rule
   {
-    name="RDP"
+    name="Http"
     priority=1004
     direction="Inbound"
     access="Allow"
     protocol="Tcp"
     source_port_range="*"
-    destination_port_range="3389"
+    destination_port_range="80"
     source_address_prefix="*"
     destination_address_prefix="*"
   }
