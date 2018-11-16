fb0fd3be9c4ed2cce685ba0123cf98a69b6d835f
Ubuntu <azureops@table5radevopsvm.4l3lugxq3xcudo0ibo4qljcaff.gx.internal.cloudapp.net>
2018-11-16 15:01:04 +0000

    fix hosts script again

--- azure/hosts.sh
+++ azure/hosts.sh
@@ -2,7 +2,7 @@
 
 vm_ip=$(terraform output -state=/ssh/terraform.tfstate vm_ip)
 echo $vm_ip
-echo "host1 ansible_user=azureops ansible_ssh_host="$vm_ip > /ssh/inventory
+echo "host1 ansible_user=azureops ansible_ssh_private_key_file=/ssh/id_rsa ansible_ssh_host="$vm_ip > /ssh/inventory
 echo "" >> /ssh/inventory
 echo "[apache]" >> /ssh/inventory
 echo "host1" >> /ssh/inventory
